57b07b920bed582e66bfd3f6be3521cc98174b06
Dartora <rodolfo.darto.da.silva@gmail.com>
2023-08-21 14:41:14 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    SideNavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
