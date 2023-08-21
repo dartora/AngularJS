d56e393424be07009b04f7f4ca4142bdf46a2f97
Dartora <rodolfo.darto.da.silva@gmail.com>
2023-08-21 20:29:05 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,31 @@ import { UsersComponent } from './Components/Users/users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SideNavigationComponent } from './Components/Sidebar/side-navigation.component';
 import { PostsComponent } from './Pages/Posts/posts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    SideNavigationComponent,
+    // SideNavigationComponent,
     PostsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
