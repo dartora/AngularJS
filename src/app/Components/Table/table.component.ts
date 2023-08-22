14dc3f26f9f8ff1c87fc0a5d5154e68003a7047c
Dartora <rodolfo.darto.da.silva@gmail.com>
2023-08-22 11:17:11 -0300

    added a actions buttons to delete and edit

--- src/app/Components/Table/table.component.ts
+++ src/app/Components/Table/table.component.ts
@@ -4,7 +4,10 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { UserService } from 'src/app/Services/user.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuTrigger } from '@angular/material/menu';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -15,16 +18,20 @@ import { MatInputModule } from '@angular/material/input';
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatMenuModule,
   ],
 })
 export class TableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['name', 'email', 'gender', 'status'];
+  displayedColumns: string[] = ['name', 'email', 'gender', 'status', 'actions'];
   dataSource = new MatTableDataSource<IUser>([]);
-  constructor(private userService: UserService) {
+
+  constructor(private userService: UserService, private router: Router) {
     this.paginator = {} as MatPaginator;
   }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
 
   ngOnInit(): void {
     this.loadUsers();
@@ -42,11 +49,29 @@ export class TableComponent implements AfterViewInit {
     );
   }
 
+  editUser(userId: number) {
+    // Implement your edit logic here
+    // For example, you can navigate to the edit page
+    // this.router.navigate(['/edit-user', userId]);
+
+    // Close the menu after action
+    this.menuTrigger.closeMenu();
+  }
+
+  deleteUser(userId: number) {
+    // Implement your delete logic here
+    // For example, you can show a confirmation dialog and delete the user if confirmed
+
+    // Close the menu after action
+    this.menuTrigger.closeMenu();
+  }
+
   ngAfterViewInit() {
     if (this.paginator) {
       this.dataSource.paginator = this.paginator;
     }
   }
+
   applyFilter(event: Event): string {
     let value = (event.target as HTMLInputElement).value; //getting target.value
     let filterValue = value.trim().toLowerCase(); // Remove whitespace and lowercase
@@ -56,6 +81,7 @@ export class TableComponent implements AfterViewInit {
 }
 
 export interface IUser {
+  id: number;
   name: string;
   email: string;
   gender: string;
