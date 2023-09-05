d5485780e48c7542e24b7d74def20f7e802a7bb0
Dartora <dartorandroid@gmail.com>
2023-09-05 13:16:45 -0300

    adding a delay to fix delete test

--- cypress/e2e/User/delete.cy.ts
+++ cypress/e2e/User/delete.cy.ts
@@ -5,6 +5,7 @@ describe('Delete User', () => {
     cy.get(
       ':nth-child(1) > .cdk-column-actions > .mat-mdc-menu-trigger > .mat-icon'
     ).click();
+    cy.wait(5000);
     cy.get('.mat-mdc-menu-content > :nth-child(2)').click();
     cy.get('[ng-reflect-dialog-result="true"]').click();
   });
