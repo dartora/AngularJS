51a797caf6e2fa729a104704f50f0ea832cb33d6
Dartora <dartorandroid@gmail.com>
2023-09-05 13:02:03 -0300

    adjusting to run e2e testes automatically after commit/deploy

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,15 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: 'https://angular-studies-iota.vercel.app',
   },
-  
-  
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+  },
+});
